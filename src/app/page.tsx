e57b34880586c6eac2221d185efb4787fca58eda
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-04-02 16:07:11 +0400

    Mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
           <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
+      <ModeToggle />
       <SignedIn>
         <UserButton />
       </SignedIn>
