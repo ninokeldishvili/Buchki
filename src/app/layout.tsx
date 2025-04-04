49d9916c22f865b5b8f4cdcd510e89ea96f50ff7
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-04-04 17:49:05 +0400

    Add sidebar for non auth user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Sidebar from "@/components/Sidebar";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,7 +25,9 @@ export default function RootLayout({
               <main className="py-8">
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                    </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
-// import { currentUser } from "@clerk/nextjs/server";
-
-// import { syncUser } from "@/actions/user.action";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
 async function Navbar() {
-  // const user = await currentUser();
-  //   if (user) await syncUser(); // POST
+  const user = await currentUser();
+  if (user) await syncUser(); // POST
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
@@ -30,5 +28,4 @@ async function Navbar() {
     </nav>
   );
 }
-
 export default Navbar;
