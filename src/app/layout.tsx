26b1339ee72d4d8f19dc64699364fbdc5eb1a1d4
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-04-08 12:32:35 +0400

    Create post functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from "@/components/Sidebar";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
@@ -33,6 +34,7 @@ export default function RootLayout({
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,13 +13,19 @@ async function Navbar() {
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-            <Link
-              href="/"
-              className="text-sm font-bold text-[#07762d] font-mono tracking-wider flex relative items-center justify-end flex-col h-full "
-            >
-              <Image src="/shamrock.png" alt="buchki" width="40" height="40" className="absolute bottom-3"/>
-              ბუჩქი
-            </Link>
+          <Link
+            href="/"
+            className="text-sm font-bold text-[#07762d] font-mono tracking-wider flex relative items-center justify-end flex-col h-full "
+          >
+            <Image
+              src="/shamrock.png"
+              alt="buchki"
+              width="40"
+              height="40"
+              className="absolute bottom-3"
+            />
+            ბუჩქი
+          </Link>
 
           <DesktopNavbar />
           <MobileNavbar />
